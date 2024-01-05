b551106e5e238cb4c43c3a5ef5f7c48b14c3861d
AuroreAile <auroredelenclos@yahoo.fr>
2024-01-05 22:04:56 +0100

    1er composant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';0
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{pokemonList[0]}}!</h1>`,
+  template: `<h1>Liste des Pokémons</h1>`,
 })
 export class AppComponent implements OnInit {
-  pokemonList: string[] = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+  pokemonList: Pokemon [] = POKEMONS;
 
   ngOnInit(): void {
     console.table(this.pokemonList);
-    this.selectPokemon('Bulbizarre')
+    this.selectPokemon(this.pokemonList[3]);
   }
 
 
-  selectPokemon(pokemonName: string): void {
-    console.log(`Vous avez cliqué sur le pokémon ${pokemonName}`);
+  selectPokemon(pokemon: Pokemon): void {
+    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
   }
 
 }
